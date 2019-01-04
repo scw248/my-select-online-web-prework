a4ef491cee65e6e2ceb1208cd3eb0adf50a5657f
Stephen Wright <scw248@gmail.com>
2019-01-04 21:56:59 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -6,7 +6,7 @@ def my_select(collection)
   while i < collection.length
   newElems = yield collection[i]
   if newElems == true
-  newArray << newElems
+  newArray << newElems[i]
 end
   i = i + 1
 end
