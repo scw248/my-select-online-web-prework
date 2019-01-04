32b96c1a5ada8a479c7865f56c4cf29004fa14f3
Stephen Wright <scw248@gmail.com>
2019-01-04 21:51:59 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  return [] if collectin.length == 0
+  
+  i = 0
+  newArray = []
+  while i < array.length
+  newElems = yield array[i]
+  newArray << newElems
+  i = i + 1
+end
+  newArray
+end
 end
